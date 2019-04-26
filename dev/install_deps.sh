edb1ca4f8f6a985b8e68181b2db057861296470f
Inigo Iparragirre <inigoiparraguirre@xn--mail-92a.com>
2019-04-26 12:38:43 +0200

    Docker compose setup

---
+++ dev/install_deps.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# 
+# File:   install-dependencies.sh
+# Author: iiparra
+#
+apt-get install -y libzmq3-dev liblog4cpp5-dev \
+libgoogle-glog-dev libboost-all-dev libssl-dev uuid-dev libzmqpp-dev libmhash-dev
